9317a59ce096811defb47875c3783da90b754752
Mahalakshmi S <mahaappu125@gmail.com>
2026-02-24 20:43:41 +0530

    Create Loop2.java

---
+++ Loop2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+    import java.io.*; 
+    class Loop2{
+        public static void main(String []argh){
+            Scanner in = new Scanner(System.in);
+            int t=in.nextInt();
+            for(int i=0;i< t;i++){
+                int a = in.nextInt();
+                int b = in.nextInt();
+                int n = in.nextInt();
+                int sum=a;
+                int r=0;
+                int o=1;
+                for(int j=0;j< n;j++)
+                {
+                 //for(int p=0;p< j;p++)
+                    r=(1<< j)*b;
+                    sum=sum+r;
+                    System.out.print(sum+" ");
+                }
+                System.out.println("");
+            }
+            
+            in.close();
+        }
+    }  
