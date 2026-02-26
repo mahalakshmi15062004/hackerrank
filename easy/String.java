bb654e4f943fe4e28ef4aafe6451d886a45d2da6
Mahalakshmi S <mahaappu125@gmail.com>
2026-02-26 15:56:00 +0530

    Update String.java

--- easy/String.java
+++ easy/String.java
@@ -5,7 +5,7 @@
             Scanner sc=new Scanner(System.in);
             String A=sc.next();
             String B=sc.next();
-            /* Enter your code here. Print output to STDOUT. */
+        
             System.out.println(A.length()+B.length());
             if(A.compareTo(B)>0)System.out.println("Yes");
             else System.out.println("No");
