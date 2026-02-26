f0c84d2e3700afcb7f317cc0e9abe1ed97775923
Mahalakshmi S <mahaappu125@gmail.com>
2026-02-26 15:55:46 +0530

    StdinaStdout.java

--- easy/StdinaStdout.java
+++ easy/StdinaStdout.java
@@ -6,7 +6,7 @@
             double d =scan.nextDouble();
             String s1 =scan.nextLine(); 
             String s =scan.nextLine(); 
-            // Write your code here.
+          
                 System.out.println("String: " + s);
             System.out.println("Double: " + d);
             System.out.println("Int: " + i);
