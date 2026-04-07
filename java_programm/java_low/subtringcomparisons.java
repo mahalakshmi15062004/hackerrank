9a72cb4ab778e5e40ee3d37f5d7515b5c68b28bd
Mahalakshmi S <mahaappu125@gmail.com>
2026-04-07 09:35:02 +0530

    Add getSmallestAndLargest method for substring comparison
    Implement method to find smallest and largest substrings of given length.

---
+++ java_programm/java_low/subtringcomparisons.java
@@ -0,0 +1,25 @@
+public static String getSmallestAndLargest(String s, int k) {
+      
+        String current = s.substring(0, k);
+        String smallest = current;
+        String largest = current;
+        
+      
+        for (int i = 1; i <= s.length() - k; i++) {
+            current = s.substring(i, i + k);
+            
+            if (current.compareTo(smallest) < 0) {
+                smallest = current;
+            }
+            if (current.compareTo(largest) > 0) {
+                largest = current;
+            }
+        }
+        
+        return smallest + "\n" + largest;
+    }
+
+    
+
+   
+
